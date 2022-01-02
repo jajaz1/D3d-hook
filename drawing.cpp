761f23fbe12c5c381736563da06d79d9163f8ae2
jajaz1 <94967729+jajaz1@users.noreply.github.com>
2022-01-02 18:37:36 +0300

    Update drawing.cpp

--- drawing.cpp
+++ drawing.cpp
@@ -1,7 +1,5 @@
 #include "includes.h"
-//https://guidedhacking.com/threads/ezp1z1s-csgo-direct3d9-hooking-esp-tutorial-series.14570/
 
-// filled rectangle
 void DrawFilledRect(int x, int y, int w, int h, D3DCOLOR color) {
 	D3DRECT rect = { x,y,x + w,y + h };
 	pDevice->Clear(1, &rect, D3DCLEAR_TARGET, color, 0, 0);
